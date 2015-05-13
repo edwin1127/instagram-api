c9d9f68797b29cf78631062399b55e738787dbb5
Edwin Corzo <edwin11ramirez@gmail.com>
2015-05-13 11:55:32 -0700

    video 9

--- index.php
+++ index.php
@@ -11,6 +11,22 @@ define('clientSecret', '5a583912807c43c4a56b19a3d3469739');
 define('redirectURI', 'http://localhost/appacademyapi/index.php');
 define('ImageDirectory', 'pics/');
 
+//fucntion that is going to connect to instagram.
+function connectToinstagram($url){
+	$ch = curl_init();
+
+	curl_setopt_array($ch, array(
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_SSL_VERIFYHOST => 2,
+	));
+	$result = curl_exec($ch);
+	curl_close($ch);
+	return $result;
+
+}
+
 if (isset($_GET['code'])) {
 	$code = ($_GET['code']);
 	$url = "https://api.instagram.com/oauth/access_token";
